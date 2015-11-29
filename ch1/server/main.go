60f9da5ec6dd8888ef70b15670755e4701d3a82f
Jim Lawton <jim.lawton@gmail.com>
2015-11-29 18:02:43 +0000

    Log request info rather than echoing.

--- ch1/server/main.go
+++ ch1/server/main.go
@@ -21,17 +21,17 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	mu.Lock()
 	count++
 	mu.Unlock()
-	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+	log.Printf("%s %s %s\n", r.Method, r.URL, r.Proto)
 	for k, v := range r.Header {
-		fmt.Fprintf(w, "Header[%q]: %q\n", k, v)
+		log.Printf("Header[%q]: %q\n", k, v)
 	}
-	fmt.Fprintf(w, "Host: %q\n", r.Host)
-	fmt.Fprintf(w, "RemoteAddr: %q\n", r.RemoteAddr)
+	log.Printf("Host: %q\n", r.Host)
+	log.Printf("RemoteAddr: %q\n", r.RemoteAddr)
 	if err := r.ParseForm(); err != nil {
 		log.Print(err)
 	}
 	for k, v := range r.Form {
-		fmt.Fprintf(w, "Form[%q]: %q\n", k, v)
+		log.Printf("Form[%q]: %q\n", k, v)
 	}
 }
 
