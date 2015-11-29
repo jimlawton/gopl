601d910ace82e2495d674025e85b175dfd23af27
Jim Lawton <jim.lawton@gmail.com>
2015-11-29 13:51:37 +0000

    Exercise 1.4.

--- ch1/dup/dup.go
+++ ch1/dup/dup.go
@@ -8,9 +8,10 @@ import (
 
 func main() {
 	counts := make(map[string]int)
+	cfiles := make(map[string][]string)
 	files := os.Args[1:]
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+		countLines(os.Stdin, counts, nil)
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
@@ -18,20 +19,34 @@ func main() {
 				fmt.Fprintf(os.Stderr, "dup: %v\n", err)
 				continue
 			}
-			countLines(f, counts)
+			countLines(f, counts, cfiles)
 			f.Close()
 		}
 	}
 	for line, n := range counts {
 		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+			if len(files) == 0 {
+				fmt.Printf("%d\t%s\n", n, line)
+			} else {
+				fmt.Printf("%d\t%s\t%v\n", n, line, cfiles[line])
+			}
 		}
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, counts map[string]int, files map[string][]string) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
 		counts[input.Text()]++
+		seen := false
+		for _, fname := range files[input.Text()] {
+			if fname == f.Name() {
+				seen = true
+				break
+			}
+		}
+		if !seen {
+			files[input.Text()] = append(files[input.Text()], f.Name())
+		}
 	}
 }
