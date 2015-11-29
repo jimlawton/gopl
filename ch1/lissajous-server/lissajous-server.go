840823a03f7188df101dfe2b10fbfc2f8bb76471
Jim Lawton <jim.lawton@gmail.com>
2015-11-29 22:50:37 +0000

    Use URL queries to drive Lissajous. This only works for one query key/value pair. Don't know why yet.

--- ch1/lissajous-server/lissajous-server.go
+++ ch1/lissajous-server/lissajous-server.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"image"
 	"image/color"
 	"image/gif"
@@ -10,16 +9,14 @@ import (
 	"math"
 	"math/rand"
 	"net/http"
-	"os"
+	"strconv"
 	"time"
 )
 
-const (
-	bgIndex = 0     // background color palette index
-	fgIndex = 1     // foreground color palette index
-	cycles  = 5     // default number of complete x oscillator revolutions
-	res     = 0.001 // default angular resolution
+var (
+	cycles  = 5.0   // default number of complete x oscillator revolutions
 	size    = 250   // default image canvas covers [-size..+size]
+	res     = 0.001 // default angular resolution
 	nFrames = 64    // default number of animation frames
 	delay   = 8     // default delay between frames in 10ms units
 )
@@ -27,10 +24,33 @@ const (
 func main() {
 	log.Print("Server running...")
 	rand.Seed(time.Now().UTC().UnixNano())
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		lissajous(w)
-	})
+	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%s %s %s\n", r.Method, r.URL, r.Proto)
+	if x, err := strconv.ParseFloat(r.FormValue("cycles"), 64); err == nil {
+		cycles = x
+	}
+	if x, err := strconv.Atoi(r.FormValue("size")); err == nil {
+		size = x
+	}
+	if x, err := strconv.ParseFloat(r.FormValue("res"), 64); err == nil {
+		res = x
+	}
+	if x, err := strconv.Atoi(r.FormValue("nframes")); err == nil {
+		nFrames = x
+	}
+	if x, err := strconv.Atoi(r.FormValue("delay")); err == nil {
+		delay = x
+	}
+	log.Printf("cycles=%v\n", cycles)
+	log.Printf("size=%v\n", size)
+	log.Printf("res=%v\n", res)
+	log.Printf("nFrames=%v\n", nFrames)
+	log.Printf("delay=%v\n", delay)
+	lissajous(w)
 }
 
 func lissajous(out io.Writer) {
@@ -39,7 +59,6 @@ func lissajous(out io.Writer) {
 		red := (uint8)((i * 255) / nFrames)
 		green := (uint8)(((i + nFrames/2) * 255) / nFrames)
 		blue := (uint8)(((nFrames/2 - i) * 255) / nFrames)
-		fmt.Fprintf(os.Stderr, "RGB=(0x%02x%02x%02x)\n", red, green, blue)
 		palette = append(palette, color.RGBA{red, green, blue, 0xff})
 	}
 	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
@@ -51,8 +70,9 @@ func lissajous(out io.Writer) {
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
-				(uint8)(i+1))
+			xcoord := size + int(x*(float64)(size)+0.5)
+			ycoord := size + int(y*(float64)(size)+0.5)
+			img.SetColorIndex(xcoord, ycoord, (uint8)(i+1))
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
