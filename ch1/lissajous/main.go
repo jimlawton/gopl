31da22b01a0e538e5dd8dcaa36c2f57fe3489f5b
Jim Lawton <jim.lawton@gmail.com>
2015-11-29 15:10:43 +0000

    Exercise 1.5 - green on black.

--- ch1/lissajous/main.go
+++ ch1/lissajous/main.go
@@ -11,11 +11,16 @@ import (
 	"time"
 )
 
-var palette = []color.Color{color.White, color.Black}
+var palette = []color.Color{color.Black, color.RGBA{0, 0xff, 0, 0xff}}
 
 const (
-	whiteIndex = 0 // first color in palette
-	blackIndex = 1 // next color in palette
+	bgIndex = 0     // background color palette index
+	fgIndex = 1     // foreground color palette index
+	cycles  = 5     // number of complete x oscillator revolutions
+	res     = 0.001 // angular resolution
+	size    = 100   // image canvas covers [-size..+size]
+	nFrames = 64    // number of animation frames
+	delay   = 8     // delay between frames in 10ms units
 )
 
 func main() {
@@ -24,28 +29,21 @@ func main() {
 }
 
 func lissajous(out io.Writer) {
-	const (
-		cycles  = 5     // number of complete x oscillator revolutions
-		res     = 0.001 // angular resolution
-		size    = 100   // image canvas covers [-size..+size]
-		nframes = 64    // number of animation frames
-		delay   = 8     // delay between frames in 10ms units
-	)
 	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
-	anim := gif.GIF{LoopCount: nframes}
-	phase := 0.0 // phase difference
-	for i := 0; i < nframes; i++ {
+	anim := gif.GIF{LoopCount: nFrames}
+	phase := 0.0
+	for i := 0; i < nFrames; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
 			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
-				blackIndex)
+				fgIndex)
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
 		anim.Image = append(anim.Image, img)
 	}
-	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+	gif.EncodeAll(out, &anim)
 }
